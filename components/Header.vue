356461fc37fee73f6c6b8389bbd4f592f42b6f38
Maria Mora <mariamora2807@gmail.com>
2021-01-09 15:33:59 -0500

    Init platzi-music-nuxt project

---
+++ components/Header.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+    section.hero.is-success
+        .hero-head
+            header
+                nav.navbar(role="navigation", aria-label="main navigation")
+                    .navbar-brand
+                        strong.navbar-item 🎶 Platzi Music
+                        a.navbar-burger(@click="isActive" role="button", aria-label="menu", aria-expanded="false", data-target="navMenu")
+                            span(aria-hidden="true")
+                            span(aria-hidden="true")
+                            span(aria-hidden="true")
+                    div#navMenu.navbar-menu(:class="{'is-active': isBurgerActive }")
+                        .navbar-start
+                            router-link.navbar-item(to="search") Search
+                            router-link.navbar-item(to="about") About us
+                        .navbar-end
+            .hero-body
+                .container.has-text-centered
+                    h1.title Platzi Music
+                    h2.subtitle Songs so good!
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isBurgerActive: false
+        };
+    },
+    methods: {
+        isActive() {
+            this.isBurgerActive = !this.isBurgerActive;
+        }
+    }
+};
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  div
+    pm-header
+    nuxt
+    pm-footer
+</template>
+
+<script>
+import PmHeader from '~/components/Header.vue'
+import PmFooter from '~/components/Footer.vue'
+
+export default {
+  components: {
+    PmHeader,
+    PmFooter
+  }
+}
+</script>
+
+<style>
+
+</style>
+
